f4fab4ef3dc749d15872ab0f74a0e09b83136611
Kamil <29171@uwr.edu.pl>
2021-01-23 16:08:01 +0100

    Stripe added to charge controller's logic

--- app/controllers/charge_controller.rb
+++ app/controllers/charge_controller.rb
@@ -5,7 +5,18 @@ class ChargeController < ApplicationController
              redirect_to root_path
              return
           end
-
+	  #setting up Stripe session for payment
+	  @session = Stripe::Charge::Session.create(
+                        payment_method_types: ['card'],
+                        line_items: [{
+                          title: event.title,
+                          amount: event.price,
+                          currency: 'eur',
+                          quantity: 1
+                        }],
+                        success_url: charge_success_url,
+                        cancel_url: charge_cancel_url
+                )
           respond_to do |format|
             format.js #render create.js.erb
 	  end
