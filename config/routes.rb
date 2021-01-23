8b9181aeb96cf3a31ba8fa18edab187c51b5ef25
Kamil <29171@uwr.edu.pl>
2021-01-23 15:34:18 +0100

    Charge Controller initialization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+#Obsluga kupna ticketu na event
+  scope 'charge' do
+        post 'create', to: 'charge#create', as: 'charge_create'
+        get 'cancel', to: 'charge#cancel', as: 'charge_cancel'
+        get 'success', to: 'charge#success', as: 'charge_success'
+        end
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
